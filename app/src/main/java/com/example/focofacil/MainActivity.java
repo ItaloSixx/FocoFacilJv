3c120f16dedab14566c18ae76ce0c99aba23f6f4
ItaloSixx <anonimosbang@gmail.com>
2024-02-16 16:06:14 -0300

    DB Room Library

--- app/src/main/java/com/example/focofacil/MainActivity.java
+++ app/src/main/java/com/example/focofacil/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        // Remove a linha abaixo se não desejar definir um tema específico
-        // setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
     }
 }
