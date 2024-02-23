c802f2f47de28e7bc66b20c3a8a68c623f0765bb
Daniel Oliveira <dannielhyuuga2014@gmail.com>
2024-02-23 16:27:51 -0300

    Bottom app bar + drawer menu 100%

--- app/src/main/java/com/example/focofacil/CadastrarDiaActivity.java
+++ app/src/main/java/com/example/focofacil/CadastrarDiaActivity.java
@@ -1,16 +1,6 @@
 package com.example.focofacil;
-import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,10 +13,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class CadastrarDiaActivity extends AppCompatActivity {
-
-    FloatingActionButton fab;
     DrawerLayout drawerLayout;
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +40,6 @@ public class CadastrarDiaActivity extends AppCompatActivity {
             navigationView.setCheckedItem(R.id.nav_perfil);
         }
 
-        replaceFragment(new CadastrarDiaFragment());
-        bottomNavigationView.setBackground(null);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home || item.getItemId() == R.id.ClipBoard || item.getItemId() == R.id.Perfil) {
                 replaceFragment(new CadastrarDiaFragment());
@@ -62,66 +49,10 @@ public class CadastrarDiaActivity extends AppCompatActivity {
 
     }
 
-    private  void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
-    }
-
-    private void showBottomDialog() {
-
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.botlayout);
-
-        LinearLayout videoLayout = dialog.findViewById(R.id.layoutHome);
-        LinearLayout shortsLayout = dialog.findViewById(R.id.layoutClipBoard);
-        LinearLayout liveLayout = dialog.findViewById(R.id.layoutPerfil);
-        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
-
-        videoLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(CadastrarDiaActivity.this, "Home foi clicado", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        shortsLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(CadastrarDiaActivity.this, "Clipboard List foi Clicado", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        liveLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(CadastrarDiaActivity.this, "Perfil foi clicado", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
-
     }
 }
--- app/src/main/java/com/example/focofacil/CadastrarDiaFragment.java
+++ app/src/main/java/com/example/focofacil/CadastrarDiaFragment.java
@@ -1,64 +1,65 @@
 package com.example.focofacil;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CadastrarDiaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
 public class CadastrarDiaFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CadastrarDiaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CadastrarDiaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CadastrarDiaFragment newInstance(String param1, String param2) {
-        CadastrarDiaFragment fragment = new CadastrarDiaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Toolbar toolbar;
+    private TextView txtOl;
+    private TextView txtNomeUsuario;
+    private ImageView imageGroup142;
+    private Spinner diaSpinner;
+    private TextView txtSistemasEmbarc;
+    private TextView txtHoras;
+    private TextView txt10201200;
+    private View lineLineOne;
+    private View lineLineTwo;
+    private TextView txtProtocolosWeb;
+    private TextView txtHorasOne;
+    private TextView txt15301710;
+    private TextView txtAdicionarclass;
+    private EditText etGroupTwentyNine;
+    private EditText etGroupThirtyThree;
+    private EditText etGroupThirtyOne;
+    private AppCompatButton btnAdicionarClasseOne;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cadastrar_dia, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastrar_dia, container, false);
+        toolbar = view.findViewById(R.id.toolbarToolbar);
+        txtOl = view.findViewById(R.id.txtOl);
+        txtNomeUsuario = view.findViewById(R.id.txtNomeUsuario);
+        imageGroup142 = view.findViewById(R.id.imageGroup142);
+        diaSpinner = view.findViewById(R.id.diaSpinner);
+        txtSistemasEmbarc = view.findViewById(R.id.txtSistemasEmbarc);
+        txtHoras = view.findViewById(R.id.txtHoras);
+        txt10201200 = view.findViewById(R.id.txt10201200);
+        lineLineOne = view.findViewById(R.id.lineLineOne);
+        lineLineTwo = view.findViewById(R.id.lineLineTwo);
+        txtProtocolosWeb = view.findViewById(R.id.txtProtocolosWeb);
+        txtHorasOne = view.findViewById(R.id.txtHorasOne);
+        txt15301710 = view.findViewById(R.id.txt15301710);
+        txtAdicionarclass = (AppCompatButton) view.findViewById(R.id.btnAdicionarClasseOne);// Corrigido para AppCompatButton
+        etGroupTwentyNine = view.findViewById(R.id.etGroupTwentyNine);
+        etGroupThirtyThree = view.findViewById(R.id.etGroupThirtyThree);
+        etGroupThirtyOne = view.findViewById(R.id.etGroupThirtyOne);
+        btnAdicionarClasseOne = view.findViewById(R.id.btnAdicionarClasseOne);
+
+        return view;
     }
 }
