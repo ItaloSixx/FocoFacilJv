c5b1c8c78af69e05a3efd7012321e24ed8853a43
Daniel Oliveira <dannielhyuuga2014@gmail.com>
2024-03-02 11:13:57 -0300

    Correções de erros e estilização

--- app/src/main/java/com/example/focofacil/Activity/MainMenuActivity.java
+++ app/src/main/java/com/example/focofacil/Activity/MainMenuActivity.java
@@ -27,6 +27,7 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
 
+
         drawerLayout = findViewById(R.id.drawer_layout);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -45,8 +46,6 @@ public class MainMenuActivity extends AppCompatActivity {
         }
 
         replaceFragment(new CadastrarDiaFragment());
-
-        bottomNavigationView.setBackground(null);
         bottomNavigationView.setOnItemSelectedListener(item -> {
 
             int itemId = item.getItemId();
@@ -63,18 +62,11 @@ public class MainMenuActivity extends AppCompatActivity {
 
     }
 
-    private  void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
 
-    private void showBottomDialog() {
-
-    }
-
-
-
-
-    }
+}
